d5e7e511e8bdd9ba22250833dfae3fb439077e0e
Felipe Bicca <felipedbicca@hotmail.com>
2022-05-16 15:19:08 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8081"
+    baseUrl: "http://localhost:8081",
+    BucketBaseUrl: "https://curso-spring-ionic-88.s3.sa-east-1.amazonaws.com"
 }
