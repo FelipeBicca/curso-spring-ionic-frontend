2038ce06d5f0c002c63119034ddc78949da23df3
Felipe Bicca <felipedbicca@hotmail.com>
2022-05-23 14:32:01 -0300

    Carregando dados do produto

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -35,8 +35,7 @@ export class ProdutosPage {
     }
   }
 
-  showDetail(){
-    console.log("Detail")
-    this.navCtrl.push("ProdutoDetailPage");
+  showDetail(produto_id: string){
+    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
   }
 }
