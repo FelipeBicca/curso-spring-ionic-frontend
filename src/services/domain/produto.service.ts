76d707615104bf4fa69ec817dec50b1d398530d5
Felipe Bicca <felipedbicca@hotmail.com>
2022-05-23 13:17:53 -0300

    Carregando imagens dos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,15 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { API_CONFIG } from "../../config/api.config";
 
 @Injectable()
 export class ProdutoService{
 
     constructor(public http: HttpClient){
-
     }
 
     findByCategoria(categoria_id: string){
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+
+    getSmallImageFromBucket(id: string): Observable<any>{
+        let url = `${API_CONFIG.BucketBaseUrl}/prod${id}-small.jpg` 
+        return this.http.get(url, {responseType: 'blob'});
     }
 }
