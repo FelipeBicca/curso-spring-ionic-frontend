2d450a0e89b808c0a780bc66245e193359f3d952
Felipe Bicca <felipedbicca@hotmail.com>
2022-05-27 15:54:22 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -21,16 +21,7 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
-    let categoria_id = this.navParams.get("categoria_id");
-    let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id).subscribe(response => {
-      this.items = response['content'];
-      loader.dismiss();
-      this.loadImageUrls();
-    },
-    error => {
-      loader.dismiss();
-    });
+    this.loadData();
   }
 
   loadImageUrls(){
@@ -54,5 +45,25 @@ export class ProdutosPage {
     });
     loader.present();
     return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 500);
+  }
+
+  loadData(){
+    let categoria_id = this.navParams.get("categoria_id");
+    let loader = this.presentLoading();
+    this.produtoService.findByCategoria(categoria_id).subscribe(response => {
+      this.items = response['content'];
+      loader.dismiss();
+      this.loadImageUrls();
+    },
+    error => {
+      loader.dismiss();
+    });
   }
 }
